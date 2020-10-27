ad4c9eff0bf43fca937c48d2fe0e0ea9b5ceb4c2
Diana Martinez <36317627+dimimart@users.noreply.github.com>
2020-10-26 19:11:12 -0700

    Add files via upload

---
+++ EON.java
@@ -0,0 +1,18 @@
+//beg of code
+public class EvenOddNew {
+	public static void main(String[] args) {
+
+int a = -11;
+
+if (a % 2 == 0) {
+	System.out.println("a, which is " + a + " , is divisible by 2, therefore it is even");
+	} //end first if
+
+
+if (a % 2 != 0) {
+	System.out.println("a, which is  " + a + " , is not divisible by 2, therefore it is odd");
+	} //ends second if
+
+
+	}// end main
+} //end class
